fb9d04f1db1db54306bd8fc3d6f4c06824bf6774
Thi Giang <tuyetgiangst93@gmail.com>
2023-03-30 21:44:41 -0700

    cleansing codes

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,3 @@
-# from yelp_eda import Text_plot
-from yelp_eda import Text_preprocessing
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,11 +10,12 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.chunk import RegexpParser
 from nltk.tree import tree
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 
 def import_csvdata(file_name, header_status):
     data=pd.read_csv(file_name, header=header_status)
     return data
-
 path="/Users/gabati/Documents/GitHub/Yelp-review/yelproject/data/"
 columns_name={0:"reviews",1:"rating"}
 file_name="yelp_data.csv"
@@ -36,11 +35,37 @@ for word in revert_words:
     stop_words.remove(word)
 
 
-review=Text_preprocessing(data["reviews"]).token_word()
+
+        
+
 
 """
-REMOVING STOP WORDS as they appear a lot in the wordcloud
+Plot the distribution of the original data for a better understand of the problem.
+The figure distribution of the data shows that the dataset is unbalanced
+5 star reviews takes up to 50% of the data. Small rating (negative rating) has very few observation.
+i.e. there are <1000 review with 2 stars, about 1000 reviews with 1 star. The negative review is only 12% of the dataset
 """
-review=Text_preprocessing(data["reviews"]).remove_stop_words(stop_words)
 
-print(review)
+Yelp_eda(data).plot_bar(x=data["rating"],title="Distribution of the data",ylabel="Number of review" )
+"""
+Create wordcloud for the original data to investigate the text
+"""
+orig_reviews=Yelp_eda(data).prepro_token(data["reviews"])
+Yelp_eda(data).plot_wordcloud("WordCloud for all ratings", orig_reviews)
+Yelp_eda(data).plot_subwordcloud("token",stop_words)
+
+# """
+# REMOVING STOP WORDS as they appear a lot in the wordcloud
+# """
+removed_reviews=Yelp_eda(data).prepro_remove_stopwords(stop_words, data["reviews"])
+Yelp_eda(data).plot_wordcloud("WorldCloud for all ratings after removing stopwords", removed_reviews)
+Yelp_eda(data).plot_subwordcloud("remove",stop_words)
+
+
+# review_sentences=[]
+# for rev in removed_reviews:
+#     text=""
+#     for word in rev:
+#         text+= (" "+word)
+#     review_sentences.append(text)
+# data["cleaned_review"]=pd.DataFrame(review_sentences)
--- yelp_eda.py
+++ yelp_eda.py
@@ -11,40 +11,41 @@ from nltk.stem import WordNetLemmatizer
 from nltk.chunk import RegexpParser
 from nltk.tree import tree
 
-def import_csvdata(file_name, header_status):
-    data=pd.read_csv(file_name, header=header_status)
-    return data
-
-class Text_plot():
-    def __init__(self, data, text):
+class Yelp_eda:
+    def __init__(self,data):
         self.__data=data
-        self.__text=text
-    def dist_bar(self,x, title, ylabel):
+    def plot_bar(self,x, title, ylabel):
         fig= plt.figure
         ax=sns.countplot(x=x)
         plt.title(title)
         plt.ylabel(ylabel)
         plt.tight_layout()
         plt.show()
-    def wordcloud(self, title):
-        WC=WordCloud(max_words=10000).generate(str(self.__text))
+    def plot_wordcloud(self, title, reviews):
+        WC=WordCloud(max_words=10000).generate(str(reviews))
         fig=plt.figure
         plt.imshow(WC)
         plt.title(title)
         plt.axis('off')
         plt.show()
-        
-class Text_preprocessing():
-    def __init__(self, data):
-        self.__data=data
-    def token_word(self):
-        token_review=[word_tokenize(text) for text in self.__data]
+    def plot_subwordcloud(self, sentence_type, stop_words):
+        numsub=len(np.unique(self.__data["rating"]))
+        for i in range(1,numsub+1):
+            review=self.__data["reviews"][data['rating']==i]
+            if sentence_type=="token":
+                subreview=self.prepro_token(reviews=review)
+            elif sentence_type=="remove":
+                subreview=self.prepro_remove_stopwords(stop_words, reviews=review)
+            self.plot_wordcloud("WordCloud for reviews with rating {star} star".format(star=i), subreview)
+
+    def prepro_token(self, reviews):
+        token_review=[word_tokenize(words) for words in reviews]
         return token_review
 
-    def chunking(self, chunk_rule):
+    def prepro_chunking(self, chunk_rule):
         chunk_parser=RegexpParser(chunk_rule)
         all_chunks=[]
-        pos_tags=[nltk.pos_tag(tokens) for tokens in self.token_word(self.__data)]
+        pos_tags=[nltk.pos_tag(tokens) for tokens in self.prepro_token(self.__data["reviews"])]
         for pos_tag in pos_tags:
             tree=chunk_parser.parse(pos_tag)
             chunks=[]
@@ -53,72 +54,45 @@ class Text_preprocessing():
                     chunk=' '.join([token for token, pos in subtree.leaves()])
                     chunks.append(chunk)
             all_chunks.append(chunks)
-    def remove_stop_words(self, stop_words):        
+    def prepro_remove_stopwords(self, stop_words, reviews):       
         filter_token= []
-        for rev in self.token_word():
+        for rev in self.prepro_token(reviews):
             filter=[word for word in rev if word.lower() not in stop_words]
             filter_token.append(filter)
         return filter_token
     
-    def lemmatize_word(self):
+    def prepro_lemmatize(self):
         lemmatizer=WordNetLemmatizer()
         filter_lemma=[]
-        for rev in self.remove_stop_words(stop_words):
+        for rev in self.prepro_remove_stopwords(stop_words):
             filter=[lemmatizer.lemmatize(word) for word in rev]
             filter_lemma.append(filter)
         return filter_lemma
 
-
-path="/Users/gabati/Documents/GitHub/Yelp-review/yelproject/data/"
-columns_name={0:"reviews",1:"rating"}
-file_name="yelp_data.csv"
-review_data=import_csvdata(file_name, None) #importing data
-review_data=pd.DataFrame(review_data).rename(columns=columns_name) #rename columns for easy working
-data=review_data.drop_duplicates(subset=["reviews"],keep='first') #remove duplicates
-
-
-stop_words=set(stopwords.words('English'))
-special_char=["'ve",".", ",", "...", "[", "]", "@", "_", "!", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?",  "{", "}", "~", ":"]
-revert_words=["won't",'does','aren',"it's","don't",'isn','are',"aren't",'doesn','didn',"isn't",'ain',
-"wasn't", 'hasn','won','mightn', "shouldn't",'mustn',"mustn't","haven't","weren't",
-"didn't","doesn't",'couldn','weren', 'hadn',"hadn't",'not',"needn't","no",'shouldn',"wouldn't","very"]
-for char in special_char:
-    stop_words.add(char)
-for word in revert_words:
-    stop_words.remove(word)
-
-
-def subset_review(data, type, stop_words):
-    numsub=len(np.unique(data["rating"]))
-    for i in range(1,numsub+1):
-        preprocessing=Text_preprocessing(data["reviews"][data['rating']==i])
-        if type=="token":
-            subreview=preprocessing.token_word()
-        elif type=="remove":
-            subreview=preprocessing.remove_stop_words(stop_words)
-        Text_plot(data, subreview).wordcloud("WordCloud for reviews with rating {star} star".format(star=i))
-        
-
-
 """
-Plot the distribution of the original data for a better understand of the problem.
-The figure distribution of the data shows that the dataset is unbalanced
-5 star reviews takes up to 50% of the data. Small rating (negative rating) has very few observation.
-i.e. there are <1000 review with 2 stars, about 1000 reviews with 1 star. The negative review is only 12% of the dataset
+Feature Extraction using BoW and TF-IDF
 """
-Text_plot(data, data["reviews"]).dist_bar(x=data["rating"],title="Distribution of the data",ylabel="Number of review" )
-"""
-Create wordcloud for the original data to investigate the text
-"""
-review=Text_preprocessing(data["reviews"]).token_word()
-Text_plot(data, review).wordcloud("WordCloud for all ratings")
-subset_review(data,"token",stop_words)
 
-"""
-REMOVING STOP WORDS as they appear a lot in the wordcloud
-"""
-review=Text_preprocessing(data["reviews"]).remove_stop_words(stop_words)
-Text_plot(data, review).wordcloud("WorldCloud for all ratings after removing stopwords")
-subset_review(data,"remove",stop_words)
+class Yelp_featuresextr:
+    def __init__(self, features):
+        self.__features=features
+    def BoW(self):
+        BoW_vectorizer=CountVectorizer()
+        BoW_features=BoW_vectorizer.fit_transform(self.__features)
+        return BoW_features
+    def Tfidf(self):
+        tfidf_vectorizer=TfidfVectorizer()
+        tfidf_features=tfidf_vectorizer.fit_transform(self.__features)
+        return tfidf_features
+    def combined(self):
+        BoW_features=self.BoW()
+        tfidf_features=self.Tfidf()
+        combined_features=BoW_features.multiply(tfidf_features)
+        return combined_features
 
-print(review)
+
+
+
+
+
+
